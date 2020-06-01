86783fb59cfb60d1e7db93020dda243b02af7fbd
Jason L Perry <jason@suncoast.io>
2020-06-01 09:47:35 -0400

    Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,13 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        // sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        serif: ['chaparral-pro', ...defaultTheme.fontFamily.serif],
+        titling: ['jaf-domus-titling-web', ...defaultTheme.fontFamily.sans],
+        display: ['jaf-domus', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [require('@tailwindcss/ui')],
+  plugins: [require('@tailwindcss/ui'), require('tailwindcss-typography')],
   purge: ['./src/**/*.html'],
 }
